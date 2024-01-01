648e4afcf445dd3bdc5e81ed6a3e70e859507961
bartosz.lagoda <composit095@gmail.com>
2024-01-01 11:21:25 +0100

    Czwarta poprawka dla env w db w pliku images.tf

--- terraform/zad3/images.tf
+++ terraform/zad3/images.tf
@@ -30,9 +30,9 @@ resource "docker_container" "db" {
   name = "db"
   image = "${docker_image.postgres.id}"
   network_mode = "tfnet"
-  build_arg = {
-    POSTGRES_DB = "app"
-    POSTGRES_USER = "app_user"
-    POSTGRES_PASSWORD = "app_pass"
-  }
+  env = [
+    "POSTGRES_DB=app",
+    "POSTGRES_USER=app_user",
+    "POSTGRES_PASSWORD=app_pass"
+  ]
 }
