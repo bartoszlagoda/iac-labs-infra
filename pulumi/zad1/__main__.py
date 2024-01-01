d7937945ee406e6e1d08c6c7a18b83e3c8a229d5
bartosz.lagoda <composit095@gmail.com>
2024-01-01 16:23:15 +0100

    Pierwsza modyfikacja pliku __main__.py dla pulumi

--- pulumi/zad1/__main__.py
+++ pulumi/zad1/__main__.py
@@ -1,10 +1,33 @@
 """An AWS Python Pulumi program"""
 
-import pulumi
-from pulumi_aws import s3
+public_access_block = s3.BucketPublicAccessBlock(
+  'public-access-block', 
+  bucket=bucket.id, 
+  block_public_acls=False
+)
+def public_read_policy_for_bucket(bucket_name):
+  return pulumi.Output.json_dumps({
+      "Version": "2012-10-17",
+      "Statement": [{
+          "Effect": "Allow",
+          "Principal": "*",
+          "Action": [
+              "s3:GetObject"
+          ],
+          "Resource": [
+              pulumi.Output.format("arn:aws:s3:::{0}/*", bucket_name),
+          ]
+      }]
+  })
+s3.BucketPolicy('bucket-policy',
+  bucket=bucket.id,
+  policy=public_read_policy_for_bucket(bucket.id), 
+  opts=pulumi.ResourceOptions(depends_on=[public_access_block])
+)
 
-# Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket')
-
-# Export the name of the bucket
-pulumi.export('bucket_name', bucket.id)
+bucketObject = s3.BucketObject(
+  'index.html',
+  content_type='text/html',
+  bucket=bucket.id,
+  source=pulumi.FileAsset('index.html'),
+)
