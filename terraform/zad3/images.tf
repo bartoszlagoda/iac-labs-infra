d79ed3d57a994d78beda2f6a196f00c9ac9a583d
bartosz.lagoda <composit095@gmail.com>
2024-01-01 01:42:17 +0100

    zadanie 3

--- terraform/zad3/images.tf
+++ terraform/zad3/images.tf
@@ -10,8 +10,41 @@ resource "docker_image" "example_app" {
       author : "student"
     }
   }
+
+  env = {
+    POSTGRES_DB       = "app",
+    POSTGRES_USER     = "app_user",
+    POSTGRES_PASSWORD = "app_pass",
+  }
+
+  network {
+    mode    = "bridge"
+    networks = [docker_network.tfnet.name]
+  }
+
+  depends_on = [docker_image.postgres]
 }
 
-# resource "docker_image" "postgres" {
-#   Uzupenij
-# }
+resource "docker_network" "tfnet" {
+  name = "tfnet"
+}
+
+resource "docker_image" "postgres" {
+  name = "postgres:latest"
+}
+
+resource "docker_container" "db" {
+  name  = "my_postgres_db"
+  image = docker_image.postgres.image_id
+
+  env = {
+    POSTGRES_DB       = "my_database",
+    POSTGRES_USER     = "my_user",
+    POSTGRES_PASSWORD = "my_password",
+  }
+
+  ports {
+    internal = 5432
+    external = 5432
+  }
+}
