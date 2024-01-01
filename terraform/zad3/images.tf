eb479593d57208f22d82c85f5c7a7ba042f17d69
bartosz.lagoda <composit095@gmail.com>
2024-01-01 11:02:36 +0100

    Rozbudowanie pliku images.tf

--- terraform/zad3/images.tf
+++ terraform/zad3/images.tf
@@ -12,6 +12,22 @@ resource "docker_image" "example_app" {
   }
 }
 
-# resource "docker_image" "postgres" {
-#   Uzupenij
-# }
+resource "docker_image" "postgres" {
+  name = "postgres"
+  build {
+    context = "${path.cwd}/../../iac-labs/postgres"
+    tag  = ["postgres:latest"]
+    dockerfile = "${path.cwd}/../../iac-labs/postgres/Dockerfile"
+    args = {
+      POSTGRES_DB = "app"
+      POSTGRES_USER = "app_user"
+      POSTGRES_PASSWORD = "app_pass"
+    }
+  }
+}
+
+resource "docker_container" "db" {
+  name = "db"
+  image = "${docker_image.postgres.latest}"
+  network_mode = "tfnet"
+}
